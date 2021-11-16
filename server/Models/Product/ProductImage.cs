0335ddf821f0868e217b8313736013d9ebdca50b
Winser <Winser997@gmail.com>
2021-11-16 22:34:48 +0300

    Backend: products

---
+++ server/Models/Product/ProductImage.cs
@@ -0,0 +1,9 @@
+namespace server.Models.Product
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+    }
+}
